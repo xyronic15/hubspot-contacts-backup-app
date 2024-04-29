3ed2480ef6903b36668e62d2909443ac614e3ca7
xyronic15 <xyronb@gmail.com>
2024-04-29 18:42:04 -0400

    successfully retrieved contacts

--- app/jobs/backup_contacts_job.rb
+++ app/jobs/backup_contacts_job.rb
@@ -1,7 +1,23 @@
+require 'hubspot-api-client'
+
+ACCESS_TOKEN = ENV["HUBSPOT_ACCESS_TOKEN"]
+
 class BackupContactsJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform
+    begin
+      # make a client object
+      client = Hubspot::Client.new(access_token: ACCESS_TOKEN)
+
+      # get all contacts from HubSpot's API
+      response = client.crm.contacts.basic_api.get_all
+
+      # convert the response to an array
+      contacts = response.to_a
+
+    rescue StandardError => e
+      puts "Error: #{e.message}"
+    end
   end
 end
